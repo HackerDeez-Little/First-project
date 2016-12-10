d4ff672af6a2393d938ea3708e34516222b3ab6d
Hacker-Laptop <daniels.linkons@gmail.com>
2016-12-10 14:34:22 +0200

    Updated the code, just teaching... Have FUN!

--- Test Project!!!/Test Project!!!/Program.cs
+++ Test Project!!!/Test Project!!!/Program.cs
@@ -8,9 +8,52 @@ namespace Test_Project___
 {
     class Program
     {
+        class Player
+        {
+            private int _health = 100;
+            public int health
+            {
+                get
+                {
+                    return _health;
+                }
+
+                set
+                {
+                    if(value <= 0)
+                    {
+                        _health = 0;
+                    }else if (value >100)
+                        {
+                        _health = 100;
+
+                    }
+                    else
+                    {
+                        _health = value;
+                    }
+                }
+            }
+
+            public void Damage (int _dmg)
+            {
+                _health -= _dmg;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("FIRST CODE!!!");
+            Player tom = new Player();
+            Console.WriteLine(tom.health);
+            tom.health -= 200;
+            Console.WriteLine(tom.health);
+            tom.health += 400;
+            Console.WriteLine(tom.health);
+            tom.health = 50;
+            Console.WriteLine(tom.health);
+
+
+            Console.ReadKey();
         }
     }
 }
