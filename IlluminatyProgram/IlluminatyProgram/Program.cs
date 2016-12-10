878a9d198d552816a00582e546f953855fb2567f
Hacker-Laptop <daniels.linkons@gmail.com>
2016-12-10 17:43:19 +0200

    Update :)

---
+++ IlluminatyProgram/IlluminatyProgram/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlluminatyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String version = "v 1.0 "; // Version
+            Console.WriteLine("This program was code by:Boux " +version);
+
+            // All Strings will be declared there
+            Password:
+            String Password = "13"; // My 1337 Pass
+            String cin = Console.ReadLine(); // Info collector line
+            String q = "quit";
+            String Anime = "Anime";
+            String Cal = "Calculator";
+            String Full = "Fullmetalalchemist";
+
+
+
+            if (cin == Password)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Login Sucsess !!!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("1337 / Configs Loaded!!!");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("God mode on !!!");
+                Console.ResetColor();
+                goto Program;
+            }
+            if(cin == q)
+            {
+                return;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Acces Denied");
+                Console.ResetColor();
+                goto Password;
+            }
+
+        Program:
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("///////////////////////////////////////////////////");
+            Console.WriteLine("Chose the program ");
+            Console.WriteLine("///////////////////////////////////////////////////");
+            Console.ReadLine();
+
+            if(cin == Anime)
+            {
+                Console.WriteLine("ANIME PORN LOADED !!!");
+                Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Chose between Season");
+                Console.ResetColor();
+
+                Console.WriteLine("Assasination class room");
+
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Fullmetal alchemist");
+                Console.ResetColor();
+
+                if (cin == Full)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Check for code correction ");
+                    Console.ResetColor();
+                }
+
+
+            }
+
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        interface IItem
+        {
+            string name { get; set; }
+            int goldValue { get; set; }
+
+            void Equip();
+            void Sell();
+        }
+
+        interface IDamagable
+        {
+            int durability { get; set; }
+
+            void TakeDamage(int _amount);
+        }
+
+        interface IPartOfQuest
+        {
+            void TurnIn();
+        }
+
+
+        class Sword : IItem, IDamagable, IPartOfQuest
+        {
+            public string name { get; set; }
+            public int goldValue { get; set; }
+
+            public int durability { get; set; }
+
+            public Sword (string _name)
+            {
+                name = _name;
+                goldValue = 100;
+                durability = 30;
+            }
+
+            public void Equip()
+            {
+                Console.WriteLine(name + " equiped.");
+            }
+
+            public void Sell ()
+            {
+                Console.WriteLine(name + " Sold for " + goldValue + " imaginary dollars!");
+            }
+
+            public void TakeDamage(int _dmg)
+            {
+                durability -= _dmg;
+                Console.WriteLine(name + " damaged by " + _dmg + ". it now has a durability of " + durability);
+            }
+
+            public void TurnIn()
+            {
+                Console.WriteLine(name + " turtned in.");
+            }
+        }
+
+        class Axe : IItem, IDamagable, IPartOfQuest
+        {
+            public string name { get; set; }
+            public int goldValue { get; set; }
+
+            public int durability { get; set; }
+
+            public Axe(string _name)
+            {
+                name = _name;
+                goldValue = 70;
+                durability = 50;
+            }
+
+            public void Equip()
+            {
+                Console.WriteLine(name + " equiped.");
+            }
+
+            public void Sell()
+            {
+                Console.WriteLine(name + " Sold for " + goldValue + " imaginary dollars!");
+            }
+
+            public void TakeDamage(int _dmg)
+            {
+                durability -= _dmg;
+                Console.WriteLine(name + " damaged by " + _dmg + ". it now has a durability of " + durability);
+            }
+            public void TurnIn()
+            {
+                Console.WriteLine(name + "turned in (It was an axe).");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Sword sword = new Sword("Sword of Destany");
+            sword.Equip();
+            sword.TakeDamage(20);
+            sword.Sell();
+            
+
+            Console.WriteLine();
+
+            Axe axe = new Axe("Fury axe");
+            axe.Equip();
+            axe.TakeDamage(10);
+            axe.Sell();
+
+            Console.WriteLine();
+
+            // Create and inventory
+            IItem[] inventory = new IItem[2];
+            inventory [0] = sword;
+            inventory[1] = axe;
+
+            //Loop through and turn in all quest items
+            for(int i = 0; i < inventory.Length; i++)
+            {
+                IPartOfQuest questItem = inventory[i] as IPartOfQuest;
+                if(questItem != null)
+                {
+                    questItem.TurnIn();
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
